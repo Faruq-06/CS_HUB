b5b27191a705e8247789b234e0e9c7640533866f
Faruq-06 <mh.faruq06@gmail.com>
2024-02-05 01:43:03 +0600

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,55 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import MicrosoftIcon from '@mui/icons-material/Microsoft';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import StorageIcon from '@mui/icons-material/Storage';
+import MemoryIcon from '@mui/icons-material/Memory';
+import CellTowerIcon from '@mui/icons-material/CellTower';
+import AppShortcutIcon from '@mui/icons-material/AppShortcut';
+import ComputerIcon from '@mui/icons-material/Computer';
+import WifiChannelIcon from '@mui/icons-material/WifiChannel';
+import WebIcon from '@mui/icons-material/Web';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import AppleIcon from '@mui/icons-material/Apple';
+import InsightsIcon from '@mui/icons-material/Insights';
+
+export const logo = 'https://i.ibb.co/z8vYG5K/cover.png';
+
+export const categories = [
+  { name: 'Computer Science Home', icon: <HomeIcon />, },
+  { name: 'C programing', icon: <CodeIcon />, },
+  { name: 'OOP programming in Java', icon: <CodeIcon />, },
+  { name: 'Python Programming', icon: <CodeIcon />, },
+  { name: 'Data Structure', icon: <AccountTreeIcon />, },
+  { name: 'Algorithm', icon: <DataObjectIcon />, },
+  { name: 'Database Management', icon: <StorageIcon />, },
+  { name: 'Microprocessor', icon: <MemoryIcon />, },
+  { name: 'Operating System', icon: <MicrosoftIcon /> },
+  { name: 'Data Communication', icon: <CellTowerIcon />, },
+  { name: 'Software Engineering', icon: <AppShortcutIcon />, },
+  { name: 'Computer Architecture', icon: <ComputerIcon />, },
+  { name: 'Computer Networking', icon: <WifiChannelIcon />, },
+  { name: 'Web Engineering', icon: <WebIcon />, },
+  { name: 'Theory of Computation', icon: <SettingsEthernetIcon />, },
+  { name: 'Mobile App Development', icon: <AppleIcon />, },
+  { name: 'Machine Learning', icon: <InsightsIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
